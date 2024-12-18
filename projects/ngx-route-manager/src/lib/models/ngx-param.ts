e7fa4f8a0899cda9a253d39f27ff5a8be08b4675
Excellence Pere <elperez247@gmail.com>
2024-12-18 14:54:48 +0100

    Using signals for updating the routes

--- projects/ngx-route-manager/src/lib/models/ngx-param.ts
+++ projects/ngx-route-manager/src/lib/models/ngx-param.ts
@@ -1,8 +1,11 @@
 import { ActivatedRoute } from "@angular/router";
 import { filter, map, Observable, of } from "rxjs";
+import { internalActivatedRoute } from "../services/ngx-route-manager.service";
+import { computed } from "@angular/core";
 
 export class NgxParam {
 
+  route = internalActivatedRoute.route;
   /**
    * Name of the param
    */
@@ -11,24 +14,28 @@ export class NgxParam {
   /**
    * Returns the current snapshoot of the value in the url route
    */
-  readonly snapshotValue: () => string = () => ''
+  readonly snapshotValue = computed(() => {
+    if (!internalActivatedRoute.route()) { return '' }
+    else {
+      return internalActivatedRoute.route()?.snapshot.paramMap.get(this.name) || ''
+    }
+  })
 
   /**
    * Listens for change on param in the route
    */
-  readonly listenForValue: () => Observable<string> = () => of('');
-
-  constructor(name: string, route?: ActivatedRoute) {
-    this.name = name;
-
-    if (route) {
-      this.snapshotValue = () => route.snapshot.paramMap.get(this.name) || '';
-
-      this.listenForValue = () => route.paramMap
-                                  .pipe(
-                                    filter(paramMap => paramMap.has(this.name)),
-                                    map(paramMap => paramMap.get(this.name) || '')
-                                  )
+  readonly listenForValue = computed(() => {
+    if (!internalActivatedRoute.route()) { return of('') }
+    else {
+      return internalActivatedRoute.route()?.paramMap
+      .pipe(
+        filter(paramMap => paramMap.has(this.name)),
+        map(paramMap => paramMap.get(this.name) || '')
+      )
     }
+  })
+
+  constructor(name: string) {
+    this.name = name;
   }
 }
--- projects/ngx-route-manager/src/lib/services/ngx-route-manager.service.ts
+++ projects/ngx-route-manager/src/lib/services/ngx-route-manager.service.ts
@@ -1,33 +1,23 @@
-import { effect, inject, Inject, Injectable, signal, Type } from '@angular/core';
+import { effect, inject, Inject, Injectable, signal } from '@angular/core';
 import { NgxRoute } from '../models/ngx-route';
 import { ActivatedRoute } from '@angular/router';
 import { NGX_ROUTE_MANAGER_CONFIG } from '../../app.module';
-import { NgxParam } from '../models/ngx-param';
+
+export const internalActivatedRoute = {
+  called: 0,
+  route: signal<ActivatedRoute | undefined>(undefined),
+  routeAdded: false,
+}
 
 @Injectable()
 export class NgxRouteManagerService<T> {
 
-  route = signal(inject(ActivatedRoute));
+  ngxRoutes = inject(NGX_ROUTE_MANAGER_CONFIG);
 
   constructor(
-    @Inject(NGX_ROUTE_MANAGER_CONFIG) private ngxRoutes: T
+    // @Inject(NGX_ROUTE_MANAGER_CONFIG) private ngxRoutes: T
   ) {
-    effect(() => {
-      try {
-        for (const key in this.ngxRoutes) {
-  
-          let ngxRoute: NgxRoute<string> = this.ngxRoutes[key] as any;
-  
-          let ngxParams: any = ngxRoute.params as any;
-  
-          for (const key in ngxParams) {
-            ngxParams[key] = new NgxParam(key, this.route());
-          }
-        }
-      } catch (error) {
-        throw new Error('Unable to reinitialize route, check the ngxRoute object and type created')
-      }
-    });
+    internalActivatedRoute.route.set(inject(ActivatedRoute));
   }
 
   get routes(): T {
@@ -39,6 +29,6 @@ export class NgxRouteManagerService<T> {
    * again to get the latest activatedRoute
    */
   public initializeRoute() {
-    this.route.set(inject(ActivatedRoute));
+    internalActivatedRoute.route.set(inject(ActivatedRoute));
   }
 }
